7f944aac3d2218df975081a6185d926f2dc8b2e8
João Fonseca <jpedrofonseca_94@hotmail.com>
2018-01-30 10:56:30 +0000

    Update PKCS11_Wrapper.py

--- M2/PKCS11_Wrapper.py
+++ M2/PKCS11_Wrapper.py
@@ -7,8 +7,8 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import Encoding
 
-# PyCKS11 homepage: https://bitbucket.org/PyKCS11/pykcs11/overview
-# PyCKS11 documentation: http://pkcs11wrap.sourceforge.net/api/
+# PyKCS11 homepage: https://bitbucket.org/PyKCS11/pykcs11/overview
+# PyKCS11 documentation: http://pkcs11wrap.sourceforge.net/api/
 
 log = logging.getLogger("Wrapper")
 
